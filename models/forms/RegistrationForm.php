290b96e3dd0b23d372b9610288a7a78537fd8595
TheMetalMonkey <toby.dunne@gmail.com>
2017-11-29 17:11:14 +1100

    registration email warning fixed

--- models/forms/RegistrationForm.php
+++ models/forms/RegistrationForm.php
@@ -28,6 +28,7 @@ class RegistrationForm extends Model
 			['username', 'unique',
 				'targetClass'     => 'webvimark\modules\UserManagement\models\User',
 				'targetAttribute' => 'username',
+			        'message' => 'Email has already been taken. If you already have an iOn account e.g. PRJL - you may login with the same email and password. If you have forgotton your password, go to Account -> Password Recovery.'
 			],
 
 			['username', 'purgeXSS'],
